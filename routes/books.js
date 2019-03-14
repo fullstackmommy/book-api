cc3c382a2443f3ca8e7111499752605315d719c3
fullstackmommy <nicole.budiman@gmail.com>
2019-03-14 18:14:26 +0800

    Test routes with supertest

--- routes/books.js
+++ routes/books.js
@@ -2,42 +2,80 @@ const express = require('express')
 const router = express.Router()
 
 const books = [
-    {
-        title: "ABC",
-        author: "Bob",
-        genre: "Computer",
-        price: 25,
-        quantity: 5
-    }, {
-        title: "DEF",
-        author: "John",
-        genre: "Arts",
-        price: 12,
-        quantity: 10
-    }, {
-        title: "GHI",
-        author: "Jane",
-        genre: "History",
-        price: 34,
-        quantity: 50
-    }
+   {
+      id: "1",
+      title: "ABC",
+      author: "Bob",
+      genre: "Computer",
+      price: 25,
+      quantity: 5
+   }, {
+      id: "2",
+      title: "DEF",
+      author: "John",
+      genre: "Arts",
+      price: 12,
+      quantity: 10
+   }, {
+      id: "3",
+      title: "GHI",
+      author: "Jane",
+      genre: "History",
+      price: 34,
+      quantity: 50
+   }
 ]
 
+verifyToken = (req, res, next) => {
+   const {authorization} = req.headers
+   if (!authorization) {
+      res.sendStatus(403)
+   } else {
+      if (authorization === "Bearer my-token") {
+         next()
+      } else {
+         res.sendStatus(403)
+      }
+   }
+}
+
 router
-    .route('/')
-    .get((req, res, next) => {
-        res.json(books)
-    })
-    .post((req, res, next) => {
-        const book = req.body
-        book.id = '214214214'
-        book.author = ''
-        book.genre = ''
-        book.price = 0
-        book.quantity = 0
-        res
-            .status(201)
-            .json(book)
-    })
+   .route('/')
+   .get((req, res, next) => {
+
+      let filteredBooks = books
+      for (const key in req.query) {
+         filteredBooks = filteredBooks.filter(book => book[key].toLowerCase().includes(req.query[key].toLowerCase()))
+      }
+      res.json(filteredBooks)
+   })
+   .post(verifyToken, (req, res) => {
+      const book = req.body
+      book.id = '214214214'
+      res
+         .status(201)
+         .json(req.body)
+   })
+
+router
+   .route('/:id')
+   .put((req, res, next) => {
+      const book = books.find(book => book.id === req.params.id)
+      if (book) {
+         res
+            .status(202)
+            .json(req.body)
+      } else {
+         res.sendStatus(400)
+      }
+   })
+   .delete((req, res) => {
+      const book = books.find(book => book.id === req.params.id)
+      if (book) {
+         res.sendStatus(202)
+      } else {
+         res.sendStatus(400)
+      }
+   })
 
 module.exports = router
--- tests/routes/books.test.js
+++ tests/routes/books.test.js
@@ -4,18 +4,21 @@ const app = require("../../app")
 
 const bookData = [
     {
+        id: "1",
         title: "ABC",
         author: "Bob",
         genre: "Computer",
         price: 25,
         quantity: 5
     }, {
+        id: "2",
         title: "DEF",
         author: "John",
         genre: "Arts",
         price: 12,
         quantity: 10
     }, {
+        id: "3",
         title: "GHI",
         author: "Jane",
         genre: "History",
@@ -35,24 +38,91 @@ describe('Books', () => {
                 .expect(bookData)
         })
 
-        test("Add a book ", () => {
+        test("Get the book with matching title", () => {
+            const title = "ABC"
+            const foundBook = bookData.filter(book => book.title === title)
+
+            return request(app)
+                .get(`${route}?title=${title}`)
+                .expect(200)
+                .expect("Content-Type", /json/)
+                .expect(foundBook)
+        })
+
+        test("Get the book with matching author", () => {
+            const author = "John"
+            const foundBook = bookData.filter(book => book.author === author)
+
+            return request(app)
+                .get(`${route}?author=${author}`)
+                .expect(200)
+                .expect("Content-Type", /json/)
+                .expect(foundBook)
+        })
+
+        test("Create a new book, deny access when no token is given ", () => {
             return request(app)
                 .post(route)
                 .send({title: "New Book"})
-                .set("Accept", "application/json")
+                .ok(res => res.status === 403)
+                .then(res => {
+                    expect(res.status).toBe(403)
+                })
+        })
+
+        test("Create a new book, deny access when invalid token is given ", () => {
+            return request(app)
+                .post(route)
+                .set("Authorization", "Bearer my-invalid-token")
+                .send({title: "New Book"})
+                .ok(res => res.status === 403)
+                .then(res => {
+                    expect(res.status).toBe(403)
+                })
+        })
+
+        test("Create a new book, grant access with authorization token ", () => {
+            return request(app)
+                .post(route)
+                .set("Authorization", "Bearer my-token")
+                .send({title: "New Book"})
                 .expect(201)
                 .then(res => {
                     expect(res.body).toEqual(expect.any(Object))
                     expect(res.body).toEqual({
                         id: expect.any(String),
-                        title: "New Book",
-                        author: expect.any(String),
-                        genre: expect.any(String),
-                        price: expect.any(Number),
-                        quantity: expect.any(Number)
+                        title: "New Book"
                     })
                 })
         })
     })
+    describe("/books/1", () => {
+        const route = "/books/1"
+        test("Update a book's record based on ID", () => {
+            return request(app)
+                .put(route)
+                .send({title: "New Title"})
+                .expect(202)
+                .expect({title: "New Title"})
+        })
 
+        test("Delete a book's record based on ID", () => {
+            return request(app)
+                .delete(route)
+                .expect(202)
+        })
+
+    })
+
+    describe("[DELETE] ", () => {
+        const route = "/books/100"
+        test("Delete a book's record based on ID, record not found", () => {
+            return request(app)
+                .delete(route)
+                .ok(res => res.status === 400)
+                .then(res => {
+                    expect(res.status).toBe(400)
+                })
+        })
+    })
 });
