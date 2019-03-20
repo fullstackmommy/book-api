5cf4eae44a6db0ecb7923d527b60d3899e70ee48
fullstackmommy <nicole.budiman@gmail.com>
2019-03-20 15:56:28 +0800

    Add routes for token

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,37 @@
+const jwt = require("jsonwebtoken")
 const express = require('express')
 const router = express.Router()
+
+const secret = "THIS IS SUPER SECRET"
+
+router
+  .route('/token')
+  .get(async(req, res) => {
+    const userData = {
+      _id: "123"
+    }
+    const expiresIn24hour = {
+      expiresIn: '24h'
+    }
+
+    const token = await jwt.sign(userData, secret, expiresIn24hour)
+    return res
+      .status(200)
+      .json({token})
+  })
+  .post(async(req, res) => {
+    if (!req.headers.authorization) {
+      res.sendStatus(401)
+    }
+    const token = req
+      .headers
+      .authorization
+      .split('Bearer ')[1]
+    const userData = await jwt.verify(token, secret)
+    return res
+      .status(200)
+      .json(userData)
+  })
 
 router.get('/', (req, res) => {
   res.sendStatus(200)
